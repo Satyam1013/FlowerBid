da7bed9496136830735fc0209675ead51a306449
Satyam Banwale <satyambanwale786@gmail.com>
2025-02-15 17:31:43 +0530

    fixed port no in index file

--- index.ts
+++ index.ts
@@ -40,9 +40,11 @@ io.on("connection", (socket: any) => {
 
 startBidCleanupCron();
 
+const PORT = process.env.PORT || 3000
+
 connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 });
 
